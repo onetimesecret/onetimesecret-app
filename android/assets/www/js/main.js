4e77d06816684fefce8e372bf1a8c03da613b6e4
delano <delano.mandelbaum@gmail.com>
2013-02-02 04:33:53 -0800

    Clipboard manager for Android (wip -- the plugin hasn't been updated for Cordorva 2 yet)

--- android/assets/www/js/main.js
+++ android/assets/www/js/main.js
@@ -1,3 +1,21 @@
+
+// Based on:
+//    https://github.com/phonegap/phonegap-plugins/tree/master/Android/ClipboardManager
+// Modified for Cordova 2 from:
+//    http://ambrusmartin.wordpress.com/2012/09/17/cordova-2-0-broken-vs-resolved/
+var ClipboardManager = function() {
+}
+
+var clipboardManager = {
+  copy: function(key, success, fail){
+    cordova.exec(success, fail, "clipboardManager", "copy", [key]);
+  },
+  paste: function(success, fail){
+    cordova.exec(success, fail, "clipboardManager", "paste", []);
+  }
+}
+
+var tmp;
 var app = {
     // Application Constructor
     initialize: function() {
@@ -9,6 +27,16 @@ var app = {
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
       document.addEventListener('deviceready', this.onDeviceReady, false);
+
+      $("#showMetadata button").on("click",function(e) {
+        tmp = e;
+        clipboardManager.copy(
+          "the text to copy",
+          function(r){alert("copy is successful")},
+          function(e){alert(e)}
+        );
+        return e.preventDefault();
+      });
       $("#createSecret").on("submit",function(e) {
         var secret = $("#createSecret textarea").val();
         if ("" == secret) {
@@ -93,5 +121,4 @@ app.parseURI = function(uri) {
   parser.href = uri;
   return parser;
 }
-
 
